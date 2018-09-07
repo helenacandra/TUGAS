086dba8cb88920e6b455d0cb3431a75e04eba449
helenacandra <43056468+helenacandra@users.noreply.github.com>
2018-09-07 12:42:08 +0700

    Add files via upload

---
+++ 11_TT3B_Helena Dwi Alex Candra_TUGAS1/src/tugas/pkg1/Tugas1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.pkg1;
+
+/**
+ *
+ * @author Helena DA Sandra
+ */
+public class Tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int jumlah = 4 ;
+        for (int i =0;i<jumlah;i++) {
+            for (int j=0; j<=i; j++){
+            System.out.print ("*");
+            }
+        System.out.println();
+        }
+    }
+    
+}
